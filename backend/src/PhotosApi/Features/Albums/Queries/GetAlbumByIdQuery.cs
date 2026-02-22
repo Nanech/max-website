0fbccec046f0a19711c583bb2d0ce68fd7c8dd4b
nane <xisaby@gmail.com>
2026-02-22 12:51:07 +0300

    feat: added new commands and queries for Album entity. Removed Category folder

--- backend/src/PhotosApi/Features/Albums/Queries/GetAlbumByIdQuery.cs
+++ backend/src/PhotosApi/Features/Albums/Queries/GetAlbumByIdQuery.cs
@@ -1,3 +1,59 @@
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using PhotosApi.Contracts;
+using PhotosApi.Infrastructure.Data;
+
 namespace PhotosApi.Features.Albums.Queries;
 
-public record GetAlbumByIdQuiery();
+public record GetAlbumByIdQuery(Guid AlbumId) : IRequest<AlbumDetailDto>;
+
+public class AlbumDetailDto(
+    Guid AlbumId,
+    string Name,
+    short? CategoryId,
+    string? CategoryName,
+    short ShootYear,
+    DateTime CreateAt,
+    List<PhotoDto> Photos
+);
+
+public record PhotoDto(
+    Guid PhotoId,
+    string Url,
+    string Status,
+    DateTime UploadedAt
+);
+
+public class GetAlbumBtIdHandler(
+    PhotosDbContext dbContext,
+    IStorageRepository storage
+) : IRequestHandler<GetAlbumByIdQuery, AlbumDetailDto>
+{
+    public async Task<AlbumDetailDto> Handle(GetAlbumByIdQuery request, CancellationToken cancellationToken)
+    {
+        var album = await dbContext.Albums
+            .Include(a => a.Category)
+            .Include(a => a.Photos)
+            .FirstOrDefaultAsync(a => a.AlbumId == request.AlbumId, cancellationToken);
+        
+        if (album == null)
+            throw new KeyNotFoundException($"Album {request.AlbumId} not found");
+
+        var photosDto = new List<PhotoDto>();
+
+        foreach (var photo in album.Photos.OrderBy(p => p.UploadedAt))
+        {
+            // var url = await storage.GetPresignedUrl(bu)
+        }
+
+        return new AlbumDetailDto(
+            album.AlbumId,
+            album.Name,
+            album.CategoryId,
+            album.Category?.Name,
+            album.ShootYear,
+            album.CreatedAt,
+            photosDto
+        );
+    }
+}
--- backend/src/PhotosApi/Features/Albums/Queries/GetAlbumsQuery.cs
+++ backend/src/PhotosApi/Features/Albums/Queries/GetAlbumsQuery.cs
@@ -1,6 +1,48 @@
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using PhotosApi.Infrastructure.Data;
+
 namespace PhotosApi.Features.Albums.Queries;
 
-public class GetAlbumsQuery
+public record GetAlbumsQuery(short? CategoryId = null) : IRequest<List<AlbumDto>>;
+
+public record AlbumDto(
+    Guid AlbumId,
+    string Name,
+    short? CategoryId,
+    string? CategoryName,
+    short ShootYear,
+    int PhotosCount,
+    DateTime CreatedAt
+);
+
+public class GetAlbumHandler(
+    PhotosDbContext dbContext,
+    ILogger<GetAlbumHandler> logger
+) : IRequestHandler<GetAlbumsQuery, List<AlbumDto>>
 {
-    
+    public async Task<List<AlbumDto>> Handle(GetAlbumsQuery request, CancellationToken cancellationToken)
+    {
+        var query = dbContext.Albums.AsQueryable();
+
+        if (request.CategoryId.HasValue)
+            query = query.Where(a => a.CategoryId == request.CategoryId.Value);
+        
+        var albums = await query
+            .Include(a => a.Category)
+            .Include(a => a.Photos)
+            .Select(a => new AlbumDto(
+                a.AlbumId,
+                a.Name,
+                a.CategoryId,
+                a.Category != null ? a.Category.Name : null,
+                a.ShootYear,
+                a.Photos.Count,
+                a.CreatedAt
+            ))
+            .OrderBy(a => a.CreatedAt)
+            .ToListAsync(cancellationToken); ;
+            
+        return albums;
+    }
 }
